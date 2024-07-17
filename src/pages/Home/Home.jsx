a4d68c5f61593a2dd47f7db33e0820787853cd74
Manifest-Son <lennoxgithinji37@gmail.com>
2024-07-17 14:09:43 +0300

    This creates the footer component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import Header from '../../components/Header/Header'
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 
 function Home() {
   return (
     <>
     <Header />
+    <Footer />
     </>
   )
 }
